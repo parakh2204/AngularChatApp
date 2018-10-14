a436808a1d755c384f662ca6bafc3346597dfe91
Parakh Maheshwari <parakh.maheshwari@hotwaxsystems.com>
2018-10-14 21:35:10 +0530

    Adding all the required files for the angular 4 chat app with firebase backend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyADaz8JxzqViBGpOTRlIJVz9e-lWDc4HOY",
+    authDomain: "angularchatapp-942be.firebaseapp.com",
+    databaseURL: "https://angularchatapp-942be.firebaseio.com",
+    projectId: "angularchatapp-942be",
+    storageBucket: "angularchatapp-942be.appspot.com",
+    messagingSenderId: "119752427622"  
+}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyADaz8JxzqViBGpOTRlIJVz9e-lWDc4HOY",
+    authDomain: "angularchatapp-942be.firebaseapp.com",
+    databaseURL: "https://angularchatapp-942be.firebaseio.com",
+    projectId: "angularchatapp-942be",
+    storageBucket: "angularchatapp-942be.appspot.com",
+    messagingSenderId: "119752427622"
+  }
+};
